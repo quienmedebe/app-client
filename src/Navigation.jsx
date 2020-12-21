871fc2d90ee85713e4aeaa24b7f164c0ee29ce31
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-12-21 12:56:16 +0100

    Complete app navigation

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,43 +1,75 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
 import {text, main, separator} from './theme/colors';
 import {Poppins} from './theme/fonts';
 
 import HeaderLeft from './components/UI/Header/HeaderLeft';
+import HeaderRight from './components/UI/Header/HeaderRight';
 
 import Overview from './pages/Overview/Overview';
 import Balance from './pages/Balance/Balance';
+import DebtList from './pages/DebtList/DebtList';
 import DebtEditor from './pages/DebtEditor/DebtEditor';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+const OverviewStack = createStackNavigator();
+const BalanceStack = createStackNavigator();
+const DebtListStack = createStackNavigator();
+
+const defaultStackNavigatorOptions = {
+  headerTitleAlign: 'center',
+  headerTintColor: text,
+  headerBackTitleVisible: false,
+  headerTitleStyle: {
+    fontFamily: Poppins.Light,
+    fontSize: 16,
+  },
+  headerStyle: {
+    backgroundColor: main,
+    shadowColor: 'transparent',
+    elevation: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: separator,
+  },
+  headerLeft: props => <HeaderLeft {...props} />,
+  headerRight: props => <HeaderRight {...props} />,
+};
+
+const defaultDrawerNavigatorOptions = {};
+
+const OverviewStackComponent = () => {
+  return (
+    <OverviewStack.Navigator initialRouteName='Overview' screenOptions={defaultStackNavigatorOptions}>
+      <OverviewStack.Screen name='Overview' component={Overview} options={{title: 'Resumen'}} />
+      <OverviewStack.Screen name='DebtEditor' component={DebtEditor} />
+    </OverviewStack.Navigator>
+  );
+};
+const BalanceStackComponent = () => {
+  return (
+    <BalanceStack.Navigator initialRouteName='Balance' screenOptions={defaultStackNavigatorOptions}>
+      <BalanceStack.Screen name='Balance' component={Balance} options={{title: 'Balance'}} />
+    </BalanceStack.Navigator>
+  );
+};
+const DebtListStackComponent = () => {
+  return (
+    <DebtListStack.Navigator initialRouteName='DebtList' screenOptions={defaultStackNavigatorOptions}>
+      <DebtListStack.Screen name='DebtList' component={DebtList} options={{title: 'Todas las deudas'}} />
+      <DebtListStack.Screen name='DebtEditor' component={DebtEditor} />
+    </DebtListStack.Navigator>
+  );
+};
 
 const Navigation = () => {
-  const defaultStackNavigatorOptions = useMemo(() => {
-    return {
-      headerTitleAlign: 'center',
-      headerTintColor: text,
-      headerBackTitleVisible: false,
-      headerTitleStyle: {
-        fontFamily: Poppins.Light,
-        fontSize: 16,
-      },
-      headerStyle: {
-        backgroundColor: main,
-        shadowColor: 'transparent',
-        elevation: 0,
-        borderBottomWidth: 1,
-        borderBottomColor: separator,
-      },
-      headerLeft: props => <HeaderLeft {...props} />,
-    };
-  }, []);
-
   return (
-    <Stack.Navigator initialRouteName='Overview' screenOptions={defaultStackNavigatorOptions}>
-      <Stack.Screen name='Overview' component={Overview} />
-      <Stack.Screen name='Balance' component={Balance} />
-      <Stack.Screen name='DebtEditor' component={DebtEditor} />
-    </Stack.Navigator>
+    <Drawer.Navigator initialRouteName='Overview' drawerPosition='right' hideStatusBar>
+      <Drawer.Screen name='Overview' component={OverviewStackComponent} options={{...defaultDrawerNavigatorOptions, title: 'Resumen'}} />
+      <Drawer.Screen name='Balance' component={BalanceStackComponent} options={{...defaultDrawerNavigatorOptions, title: 'Balance'}} />
+      <Drawer.Screen name='DebtList' component={DebtListStackComponent} options={{...defaultDrawerNavigatorOptions, title: 'Lista de deudas'}} />
+    </Drawer.Navigator>
   );
 };
 
--- src/components/UI/Header/HeaderRight.jsx
+++ src/components/UI/Header/HeaderRight.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import {View, TouchableOpacity, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {backgroundLight} from '../../../theme/colors';
 
-const HeaderLeft = ({canGoBack, onPress}) => {
-  if (!canGoBack) {
-    return null;
-  }
+const HeaderRight = () => {
+  const navigation = useNavigation();
 
   return (
     <View style={[styles.container]}>
-      <TouchableOpacity onPress={onPress}>
-        <Icon name='chevron-left' color={backgroundLight} size={36} />
+      <TouchableOpacity onPress={navigation.openDrawer}>
+        <Icon name='menu' color={backgroundLight} size={24} />
       </TouchableOpacity>
     </View>
   );
@@ -19,8 +18,8 @@ const HeaderLeft = ({canGoBack, onPress}) => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
   },
 });
 
-export default HeaderLeft;
+export default HeaderRight;
