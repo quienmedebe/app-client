8ae75ad0805c62e9c58249308c9b961d12dfbfce
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-12-21 09:48:41 +0100

    Configured app error page

--- src/components/UI/Text/H1.jsx
+++ src/components/UI/Text/H1.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components/native';
 import {Poppins} from '../../../theme/fonts';
-import {text} from '../../../theme/colors';
+import {darkText} from '../../../theme/colors';
 
 export const StyledH1 = styled.Text`
   font-size: 24px;
   font-family: ${Poppins.SemiBold};
-  color: ${text};
+  color: ${darkText};
 `;
 
 export default StyledH1;
--- src/components/UI/Text/StyledText.jsx
+++ src/components/UI/Text/StyledText.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components/native';
 import {Poppins} from '../../../theme/fonts';
+import {darkText} from '../../../theme/colors';
 
 export default styled.Text`
   font-family: ${Poppins.Regular};
   font-size: 16px;
+  color: ${darkText};
 `;
