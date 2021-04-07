c2d14ab6f7b868ab99f6549f53918dbf195be1b2
Raúl Contreras <raulcontrerasrubio@gmail.com>
2021-04-07 19:45:34 +0200

    Add isNumber tests

--- src/modules/validation/isNumber.test.jsx
+++ src/modules/validation/isNumber.test.jsx
@@ -1,7 +1,20 @@
 import isNumber from './isNumber';
 
 describe('isNumber test suite', () => {
-  test('should return true if the parameter is 10', () => {
+  test('should return true if the argument is 10', () => {
     expect(isNumber(10)).toBe(true);
+  });
+
+  test('should return false if the argument is not of the type number', () => {
+    expect(isNumber('10')).toBe(false);
+    expect(isNumber(true)).toBe(false);
+    expect(isNumber(null)).toBe(false);
+    expect(isNumber(undefined)).toBe(false);
+    expect(isNumber({})).toBe(false);
+  });
+
+  test('should return false if the argument is infinite or NaN', () => {
+    expect(isNumber(NaN)).toBe(false);
+    expect(isNumber(Infinity)).toBe(false);
   });
 });
