83a864258e882e40ddd200052cc9d0e29d47d48c
Raúl Contreras Rubio <raulcontrerasrubio@gmail.com>
2021-04-07 19:52:16 +0200

    Feature/add test configuration (#13)
    * Refactor App component. Extract PaperTheme
    
    * Update husky configuration
    
    * Add isNumber tests

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useMemo} from 'react';
+import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {ErrorBoundary} from 'react-error-boundary';
 import {Provider as ReduxProvider} from 'react-redux';
-import {configureFonts, DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import store from './src/redux/store';
 import AppManager from './src/AppManager';
 import AppError from './src/AppError';
-import {Poppins} from './src/theme/fonts';
+import {PaperTheme} from './src/theme/paper';
 import {connectDB} from './src/database';
 
 const App = () => {
@@ -18,59 +18,6 @@ const App = () => {
   useEffect(() => {
     connectDB();
   }, []);
-
-  const paperFonts = useMemo(() => {
-    return {
-      ios: {
-        regular: {
-          fontFamily: Poppins.Regular,
-          fontWeight: 'normal',
-        },
-        medium: {
-          fontFamily: Poppins.SemiBold,
-          fontWeight: 'normal',
-        },
-        light: {
-          fontFamily: Poppins.Light,
-          fontWeight: 'normal',
-        },
-        thin: {
-          fontFamily: Poppins.ExtraLight,
-          fontWeight: 'normal',
-        },
-      },
-      android: {
-        regular: {
-          fontFamily: Poppins.Regular,
-          fontWeight: 'normal',
-        },
-        medium: {
-          fontFamily: Poppins.SemiBold,
-          fontWeight: 'normal',
-        },
-        light: {
-          fontFamily: Poppins.Light,
-          fontWeight: 'normal',
-        },
-        thin: {
-          fontFamily: Poppins.ExtraLight,
-          fontWeight: 'normal',
-        },
-      },
-    };
-  }, []);
-
-  const PaperTheme = useMemo(() => {
-    return {
-      ...DefaultTheme,
-      dark: true,
-      roundness: 5,
-      fonts: configureFonts(paperFonts),
-      colors: {
-        ...DefaultTheme.colors,
-      },
-    };
-  }, [paperFonts]);
 
   return (
     <ErrorBoundary FallbackComponent={AppError}>
