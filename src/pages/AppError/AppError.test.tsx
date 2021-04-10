000e9bd02ac761cae489206a15c1d4391a09682c
Raúl Contreras <raulcontrerasrubio@gmail.com>
2021-04-10 17:32:07 +0200

    The button is reachable and pressing it fires the restartHandler function

--- src/pages/AppError/AppError.test.tsx
+++ src/pages/AppError/AppError.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
 import AppErrorView from './AppErrorView';
+
+const noop = () => {};
+
+const defaultAppErrorViewProps = {
+  restartHandler: noop,
+};
 
 describe('AppError test suite', () => {
   test('It should render without crashing', () => {
-    expect(() => render(<AppErrorView />)).not.toThrow();
+    expect(() => render(<AppErrorView {...defaultAppErrorViewProps} />)).not.toThrow();
+  });
+
+  test('It should fire the restart button on press', () => {
+    const fakeRestartHandler = jest.fn();
+    const {getByA11yLabel} = render(<AppErrorView {...defaultAppErrorViewProps} restartHandler={fakeRestartHandler} />);
+    const button = getByA11yLabel(/Restart/i);
+
+    fireEvent.press(button);
+
+    expect(fakeRestartHandler).toHaveBeenCalledTimes(1);
   });
 });
--- src/pages/AppError/AppErrorView.tsx
+++ src/pages/AppError/AppErrorView.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
-interface IProps {}
+interface IProps {
+  restartHandler: () => void;
+}
 
-const AppErrorView: React.FC<IProps> = () => {
+const AppErrorView: React.FC<IProps> = props => {
   return (
     <View>
-      <Text></Text>
+      <TouchableOpacity accessibilityLabel='Restart App' onPress={props.restartHandler}>
+        <Text>Restart App</Text>
+      </TouchableOpacity>
     </View>
   );
 };
